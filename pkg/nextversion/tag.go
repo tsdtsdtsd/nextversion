9de998170b2425e32b5b444c4d31866ac1aa820f
tsdtsdtsd <orkan.alat@googlemail.com>
2024-02-23 10:56:36 +0100

    refactor: change var name for clarity

--- pkg/nextversion/tag.go
+++ pkg/nextversion/tag.go
@@ -19,14 +19,14 @@ type Tag struct {
 // LastTag returns an annotated tag object witch has no prerelease identifier and is the greates semantic version
 func LastTag(repo *git.Repository) (*Tag, error) {
 
-	tag := Tag{
+	lastTag := Tag{
 		Commit: object.Commit{},
 		Semver: &semver.Version{},
 	}
 
 	tagRefs, err := repo.Tags()
 	if err != nil {
-		return &tag, fmt.Errorf("failed to fetch tag references: %w", err)
+		return &lastTag, fmt.Errorf("failed to fetch tag references: %w", err)
 	}
 
 	err = tagRefs.ForEach(func(ref *plumbing.Reference) error {
@@ -57,27 +57,27 @@ func LastTag(repo *git.Repository) (*Tag, error) {
 		// At this point we know that this is a valid tag object
 
 		// Store tag and commit if this is the first iteration
-		if tag.Commit.Hash.IsZero() {
-			tag.Commit = *commit
-			tag.Name = obj.Name
-			tag.Semver = semantic
-			tag.exists = true
+		if lastTag.Commit.Hash.IsZero() {
+			lastTag.Commit = *commit
+			lastTag.Name = obj.Name
+			lastTag.Semver = semantic
+			lastTag.exists = true
 			return nil
 		}
 
 		// Store tag if it's a greater version than the one we already stored
-		if semantic.GreaterThan(tag.Semver) {
-			tag.Commit = *commit
-			tag.Name = obj.Name
-			tag.Semver = semantic
-			tag.exists = true
+		if semantic.GreaterThan(lastTag.Semver) {
+			lastTag.Commit = *commit
+			lastTag.Name = obj.Name
+			lastTag.Semver = semantic
+			lastTag.exists = true
 		}
 
 		return nil
 
 	})
 
-	return &tag, err
+	return &lastTag, err
 }
 
 func (v *Tag) Exists() bool {
