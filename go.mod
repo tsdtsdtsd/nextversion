9045dfc7331f9b320da6baf082f4f18cddefd32b
tsdtsdtsd <orkan.alat@googlemail.com>
2024-02-23 10:48:38 +0100

    test: add tests for bumper

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/Masterminds/semver/v3 v3.2.1
 	github.com/go-git/go-git/v5 v5.11.0
 	github.com/leodido/go-conventionalcommits v0.12.0
+	github.com/stretchr/testify v1.8.4
 	github.com/urfave/cli/v2 v2.27.1
 )
 
@@ -16,6 +17,7 @@ require (
 	github.com/cloudflare/circl v1.3.7 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/cyphar/filepath-securejoin v0.2.4 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/emirpasic/gods v1.18.1 // indirect
 	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
 	github.com/go-git/go-billy/v5 v5.5.0 // indirect
@@ -23,6 +25,7 @@ require (
 	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
 	github.com/kevinburke/ssh_config v1.2.0 // indirect
 	github.com/pjbgf/sha1cd v0.3.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/sergi/go-diff v1.1.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
@@ -35,4 +38,5 @@ require (
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/tools v0.13.0 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ pkg/nextversion/bump_test.go
@@ -0,0 +1,298 @@
+package nextversion_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/tsdtsdtsd/nextversion/pkg/nextversion"
+)
+
+type testcase struct {
+	OldVersion      string
+	Prestable       bool
+	ChangeMajor     bool
+	ChangeMinor     bool
+	ChangePatch     bool
+	ExpectedVersion string
+}
+
+func TestCurrent(t *testing.T) {
+
+	oldVersion := "v1.0.1"
+
+	// Prestable
+	b, err := nextversion.NewBumper(oldVersion, true)
+	v := b.Current()
+
+	assert.NoError(t, err)
+	assert.Equal(t, oldVersion, v)
+
+	// Not Prestable
+	b, err = nextversion.NewBumper(oldVersion, false)
+	v = b.Current()
+
+	assert.NoError(t, err)
+	assert.Equal(t, oldVersion, v)
+}
+
+func TestPrestable(t *testing.T) {
+	testTable := []testcase{
+		// ChangeMajor = true
+		{
+			OldVersion:      "1.1.0",
+			Prestable:       true,
+			ChangeMajor:     true,
+			ChangeMinor:     true,
+			ChangePatch:     true,
+			ExpectedVersion: "v2.0.0",
+		},
+
+		{
+			OldVersion:      "0.1.0",
+			Prestable:       true,
+			ChangeMajor:     true,
+			ChangeMinor:     true,
+			ChangePatch:     true,
+			ExpectedVersion: "v0.2.0",
+		},
+		{
+			OldVersion:      "0.1.0",
+			Prestable:       true,
+			ChangeMajor:     true,
+			ChangeMinor:     true,
+			ChangePatch:     false,
+			ExpectedVersion: "v0.2.0",
+		},
+		{
+			OldVersion:      "0.1.0",
+			Prestable:       true,
+			ChangeMajor:     true,
+			ChangeMinor:     false,
+			ChangePatch:     true,
+			ExpectedVersion: "v0.2.0",
+		},
+		{
+			OldVersion:      "0.1.0",
+			Prestable:       true,
+			ChangeMajor:     true,
+			ChangeMinor:     false,
+			ChangePatch:     false,
+			ExpectedVersion: "v0.2.0",
+		},
+
+		// ChangeMajor = false
+		{
+			OldVersion:      "0.1.0",
+			Prestable:       true,
+			ChangeMajor:     false,
+			ChangeMinor:     true,
+			ChangePatch:     true,
+			ExpectedVersion: "v0.2.0",
+		},
+		{
+			OldVersion:      "0.1.0",
+			Prestable:       true,
+			ChangeMajor:     false,
+			ChangeMinor:     true,
+			ChangePatch:     false,
+			ExpectedVersion: "v0.2.0",
+		},
+		{
+			OldVersion:      "0.1.0",
+			Prestable:       true,
+			ChangeMajor:     false,
+			ChangeMinor:     false,
+			ChangePatch:     true,
+			ExpectedVersion: "v0.1.1",
+		},
+		{
+			OldVersion:      "0.1.0",
+			Prestable:       true,
+			ChangeMajor:     false,
+			ChangeMinor:     false,
+			ChangePatch:     false,
+			ExpectedVersion: "v0.1.0",
+		},
+	}
+
+	runValidChecksOnTable(t, testTable)
+}
+
+func TestNotPrestable(t *testing.T) {
+	testTable := []testcase{
+		// ChangeMajor = true
+		{
+			OldVersion:      "0.1.0",
+			Prestable:       false,
+			ChangeMajor:     true,
+			ChangeMinor:     true,
+			ChangePatch:     true,
+			ExpectedVersion: "v1.0.0",
+		},
+		{
+			OldVersion:      "0.1.0",
+			Prestable:       false,
+			ChangeMajor:     true,
+			ChangeMinor:     true,
+			ChangePatch:     false,
+			ExpectedVersion: "v1.0.0",
+		},
+		{
+			OldVersion:      "0.1.0",
+			Prestable:       false,
+			ChangeMajor:     true,
+			ChangeMinor:     false,
+			ChangePatch:     true,
+			ExpectedVersion: "v1.0.0",
+		},
+		{
+			OldVersion:      "0.1.0",
+			Prestable:       false,
+			ChangeMajor:     true,
+			ChangeMinor:     false,
+			ChangePatch:     false,
+			ExpectedVersion: "v1.0.0",
+		},
+
+		{
+			OldVersion:      "1.0.0",
+			Prestable:       false,
+			ChangeMajor:     true,
+			ChangeMinor:     true,
+			ChangePatch:     true,
+			ExpectedVersion: "v2.0.0",
+		},
+		{
+			OldVersion:      "1.0.0",
+			Prestable:       false,
+			ChangeMajor:     true,
+			ChangeMinor:     true,
+			ChangePatch:     false,
+			ExpectedVersion: "v2.0.0",
+		},
+		{
+			OldVersion:      "1.0.0",
+			Prestable:       false,
+			ChangeMajor:     true,
+			ChangeMinor:     false,
+			ChangePatch:     true,
+			ExpectedVersion: "v2.0.0",
+		},
+		{
+			OldVersion:      "1.0.0",
+			Prestable:       false,
+			ChangeMajor:     true,
+			ChangeMinor:     false,
+			ChangePatch:     false,
+			ExpectedVersion: "v2.0.0",
+		},
+
+		// ChangeMajor = false
+		{
+			OldVersion:      "0.1.0",
+			Prestable:       false,
+			ChangeMajor:     false,
+			ChangeMinor:     true,
+			ChangePatch:     true,
+			ExpectedVersion: "v0.2.0",
+		},
+		{
+			OldVersion:      "0.1.0",
+			Prestable:       false,
+			ChangeMajor:     false,
+			ChangeMinor:     true,
+			ChangePatch:     false,
+			ExpectedVersion: "v0.2.0",
+		},
+		{
+			OldVersion:      "0.1.0",
+			Prestable:       false,
+			ChangeMajor:     false,
+			ChangeMinor:     false,
+			ChangePatch:     true,
+			ExpectedVersion: "v0.1.1",
+		},
+		{
+			OldVersion:      "0.1.0",
+			Prestable:       false,
+			ChangeMajor:     false,
+			ChangeMinor:     false,
+			ChangePatch:     false,
+			ExpectedVersion: "v0.1.0",
+		},
+
+		{
+			OldVersion:      "1.0.0",
+			Prestable:       false,
+			ChangeMajor:     false,
+			ChangeMinor:     true,
+			ChangePatch:     true,
+			ExpectedVersion: "v1.1.0",
+		},
+		{
+			OldVersion:      "1.0.0",
+			Prestable:       false,
+			ChangeMajor:     false,
+			ChangeMinor:     true,
+			ChangePatch:     false,
+			ExpectedVersion: "v1.1.0",
+		},
+		{
+			OldVersion:      "1.0.0",
+			Prestable:       false,
+			ChangeMajor:     false,
+			ChangeMinor:     false,
+			ChangePatch:     true,
+			ExpectedVersion: "v1.0.1",
+		},
+		{
+			OldVersion:      "1.0.0",
+			Prestable:       false,
+			ChangeMajor:     false,
+			ChangeMinor:     false,
+			ChangePatch:     false,
+			ExpectedVersion: "v1.0.0",
+		},
+	}
+
+	runValidChecksOnTable(t, testTable)
+}
+
+func TestInvalidOldVersion(t *testing.T) {
+
+	testTable := []testcase{
+		{
+			OldVersion: "",
+		},
+		{
+			OldVersion: "abc",
+		},
+		{
+			OldVersion: "v1.0.0.0",
+		},
+		{
+			OldVersion: "v.",
+		},
+		{
+			OldVersion: "v1..",
+		},
+	}
+
+	for _, testCase := range testTable {
+		_, err := nextversion.NewBumper(testCase.OldVersion, testCase.Prestable)
+
+		assert.Error(t, err)
+	}
+}
+
+func runValidChecksOnTable(t *testing.T, testTable []testcase) {
+	for _, testCase := range testTable {
+		b, err := nextversion.NewBumper(testCase.OldVersion, testCase.Prestable)
+
+		b.CollectChange(testCase.ChangeMajor, testCase.ChangeMinor, testCase.ChangePatch)
+		v := b.Next()
+
+		assert.NoError(t, err)
+		assert.Equal(t, testCase.ExpectedVersion, v)
+	}
+}
